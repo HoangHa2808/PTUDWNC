79ee4c75acc301d5cd8d6dca07b8feac8ab0fffb
HoangHa2808 <thanha2808@gmail.com>
2023-03-08 12:46:41 +0700

    Done lab02-homework

--- src/Tips&Tricks/TatBlog.WebApp/Controllers/BlogController.cs
+++ src/Tips&Tricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -12,20 +12,28 @@ namespace TatBlog.WebApp.Controllers
             _blogRepository = blogRepository;
         }
         public async Task<IActionResult> Index(
+            [FromQuery(Name = "k")] string keyword = null,
             [FromQuery(Name = "p")] int pageNumber = 1,
             [FromQuery(Name = "ps")] int pageSize = 10)
         {
 
             var postQuery = new PostQuery()
             {
-                PublishedOnly = true
+                //Chỉ lấy những bài viết có trạng thái Published
+                PublishedOnly = true,
+
+                //Tìm bài viết theo từ khoá
+                Keyword = keyword
             };
 
-
+            //Truy vấn các bài viết theo điều kiện để tạo
             var postsList = await _blogRepository
                 .GetPagedPostQueryAsync(postQuery, pageNumber, pageSize);
+           
+            //Lưu lại điều kiện truy vấn để hiển thị trong View
             ViewBag.PostQuery = postQuery;
             
+            // Truyền danh sách bài viết vào View để render ra HTML
             return View(postsList);
         }
 
--- src/Tips&Tricks/TatBlog.WebApp/Program.cs
+++ src/Tips&Tricks/TatBlog.WebApp/Program.cs
@@ -6,76 +6,84 @@ using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
 
-
-var builder = WebApplication.CreateBuilder(args);
+namespace TatBlog.WinApp
 {
-    // Thêm các dịch vụ được yêu cầu bởi MVC Framework
-    builder.Services.AddControllersWithViews();
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            {
+                // Thêm các dịch vụ được yêu cầu bởi MVC Framework
+                builder.Services.AddControllersWithViews();
 
-    //Đăng ký các dịch vụ với DI Container
-    builder.Services.AddDbContext<BlogDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+                //Đăng ký các dịch vụ với DI Container
+                builder.Services.AddDbContext<BlogDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
 
-    builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-    builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+                builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+                builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+            }
+
+            var app = builder.Build();
+            {
+                // Cấu hình HTTP Request pipeline
+
+                // Thêm middleware để hiển thị thông báo lỗi
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Blog/Error");
+
+                    // Thêm middleware cho việc áp dụng HSTS(thêm header
+                    // Strict-Transport-Security vào HTTP Response).
+                    app.UseHsts();
+                }
+
+                // Thêm middleware dể chuyển hướng HTTP sang HTTPS
+                app.UseHttpsRedirection();
+
+                // Thêm middleware phục vụ các yêu cầu liên quan
+                // tới các tập tin nội dung tĩnh như hình ảnh, css, ...
+                app.UseStaticFiles();
+
+                // Thêm middleware lựa chọn endpoint phù hợp nhất
+                // dể xử lý một HTTP request.
+                app.UseRouting();
+
+                //Định nghĩa route template, route cóntraint cho các endpoints
+                //kết hợp với các action trong các controller
+                app.MapControllerRoute(
+                    name: "posts-by-category",
+                    pattern: "blog/category/{slug}",
+                    defaults: new { controller = "Blog", action = "Category" });
+
+                app.MapControllerRoute(
+                    name: "posts-by-tag",
+                    pattern: "blog/tag/{slug}",
+                    defaults: new { controller = "Blog", action = "Tag" });
+
+                app.MapControllerRoute(
+                   name: "posts-by-post",
+                   pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
+                   defaults: new { controller = "Blog", action = "Post" });
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Blog}/{action=Index}/{id?}");
+
+                //Thêm dữ liệu mẫu vào CSDL
+                using (var scope = app.Services.CreateScope())
+                {
+                    var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
+                    seeder.Initialize();
+                }
+            }
+            app.Run();
+        }
+    }
 }
-
-var app = builder.Build();
-{
-    // Cấu hình HTTP Request pipeline
-
-    // Thêm middleware để hiển thị thông báo lỗi
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Blog/Error");
-
-        // Thêm middleware cho việc áp dụng HSTS(thêm header
-        // Strict-Transport-Security vào HTTP Response).
-        app.UseHsts();
-    }
-
-    // Thêm middleware dể chuyển hướng HTTP sang HTTPS
-    app.UseHttpsRedirection();
-
-    // Thêm middleware phục vụ các yêu cầu liên quan
-    // tới các tập tin nội dung tĩnh như hình ảnh, css, ...
-    app.UseStaticFiles();
-
-    // Thêm middleware lựa chọn endpoint phù hợp nhất
-    // dể xử lý một HTTP request.
-    app.UseRouting();
-
-    //Định nghĩa route template, route cóntraint cho các endpoints
-    //kết hợp với các action trong các controller
-    app.MapControllerRoute(
-        name: "posts-by-category",
-        pattern: "blog/category/{slug}",
-        defaults: new { controller = "Blog", action = "Category" });
-
-    app.MapControllerRoute(
-        name: "posts-by-tag",
-        pattern: "blog/tag/{slug}",
-        defaults: new { controller = "Blog", action = "Tag" });
-
-    app.MapControllerRoute(
-       name: "posts-by-post",
-       pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
-       defaults: new { controller = "Blog", action = "Post" });
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Blog}/{action=Index}/{id?}");
-        
-//Thêm dữ liệu mẫu vào CSDL
-using (var scope = app.Services.CreateScope())
-    {
-        var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-        seeder.Initialize();
-    }
-}
-app.Run();
